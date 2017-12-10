af45b69fa9b981194249a32620ceb19605225bbd
zscgrhg <zscgrhg@qq.com>
2017-12-10 21:54:26 +0800

    ngx

--- src/main/java/com/example/nginxresource/Welcome.java
+++ src/main/java/com/example/nginxresource/Welcome.java
@@ -1,6 +1,7 @@
 package com.example.nginxresource;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.Principal;
 
@@ -32,5 +35,9 @@ public class Welcome {
             return null;
         }
     }
-
+    @GetMapping("/resource/**")
+    public void resource(HttpServletRequest request, HttpServletResponse response, @RequestHeader HttpHeaders headers) throws IOException {
+        FileInputStream file = new FileInputStream(new File("G:\\apr\\nginx-resource\\src\\main\\resources\\static\\res1\\elf.jpg"));
+        FileCopyUtils.copy(file,response.getOutputStream());
+    }
 }
